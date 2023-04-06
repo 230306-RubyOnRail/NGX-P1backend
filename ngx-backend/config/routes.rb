8e6343425d26b0977373cbb4bfa7d87b1b559dbd
Trish1996-wq <90051133+Trish1996-wq@users.noreply.github.com>
2023-04-06 16:39:35 -0400

    view/view_all

--- ngx-backend/config/routes.rb
+++ ngx-backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'reimbursement/update'
   get 'reimbursement/destroy'
+  get 'reimbursement/:id', to: 'reimbursement#show'
+  get 'reimbursement/', to:'reimbursement#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
