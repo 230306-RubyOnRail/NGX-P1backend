98bb9fec0e154cb268649e5bd8e55471ca9ce3a3
Garrett Snedeker <garrettsnedeker20@gmail.com>
2023-04-06 14:33:00 -0500

    Login feature with authentication, no authorization yet

--- ngx-backend/app/controllers/sessions_controller.rb
+++ ngx-backend/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     credentials = JSON.parse(request.body.read)
     user = User.find_by(email: credentials["email"])
     if user&.authenticate(credentials["password"])
-      render json: {token: JSONWebToken.encode(user_id: user.id), user_id: user.id}, status: :ok
+      render json: {token: JSONWebToken.encode(user_id: user.id), user_id: user.id, name: user.name}, status: :ok
     else
       render json: { error: "Invalid email or password"}, status: :unauthorized
     end
