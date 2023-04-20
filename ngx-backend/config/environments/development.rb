91e088b8a58033bf78a3ef67b53eaf6965b01317
Xien Thomas <xien.thomas@gmail.com>
2023-04-20 11:47:32 -0500

    [W] test case in workflow

--- ngx-backend/config/environments/development.rb
+++ ngx-backend/config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.hosts << "ec2-34-224-41-156.compute-1.amazonaws.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
